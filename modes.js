aea02e613a92439f88964f7845afae6e0a8abc71
Seth Daugherty <daugherty.seth@gmail.com>
2018-02-16 16:35:08 -0500

    Bunch of random stuff

---
+++ modes.js
@@ -0,0 +1,761 @@
+{
+"version": "1.0",
+"encoding": "UTF-8",
+"feed": {
+"xmlns": "http://www.w3.org/2005/Atom",
+"xmlns$openSearch": "http://a9.com/-/spec/opensearchrss/1.0/",
+"xmlns$gsx": "http://schemas.google.com/spreadsheets/2006/extended",
+"id": {
+"$t": "https://spreadsheets.google.com/feeds/list/1n7XUKOlf2n1Lw5r6A9YK8fJTIhiPHUwRM7oiZFagVds/od6/public/basic"
+},
+"updated": {
+"$t": "2017-12-04T01:29:17.418Z"
+},
+"category": [
+{
+"scheme": "http://schemas.google.com/spreadsheets/2006",
+"term": "http://schemas.google.com/spreadsheets/2006#list"
+}
+],
+"title": {
+"type": "text",
+"$t": "totals"
+},
+"link": [
+{
+"rel": "alternate",
+"type": "application/atom+xml",
+"href": "https://docs.google.com/spreadsheets/d/1n7XUKOlf2n1Lw5r6A9YK8fJTIhiPHUwRM7oiZFagVds/pubhtml"
+},
+{
+"rel": "http://schemas.google.com/g/2005#feed",
+"type": "application/atom+xml",
+"href": "https://spreadsheets.google.com/feeds/list/1n7XUKOlf2n1Lw5r6A9YK8fJTIhiPHUwRM7oiZFagVds/od6/public/basic"
+},
+{
+"rel": "http://schemas.google.com/g/2005#post",
+"type": "application/atom+xml",
+"href": "https://spreadsheets.google.com/feeds/list/1n7XUKOlf2n1Lw5r6A9YK8fJTIhiPHUwRM7oiZFagVds/od6/public/basic"
+},
+{
+"rel": "self",
+"type": "application/atom+xml",
+"href": "https://spreadsheets.google.com/feeds/list/1n7XUKOlf2n1Lw5r6A9YK8fJTIhiPHUwRM7oiZFagVds/od6/public/basic?alt=json"
+}
+],
+"author": [
+{
+"name": {
+"$t": "schism3003"
+},
+"email": {
+"$t": "schism3003@gmail.com"
+}
+}
+],
+"openSearch$totalResults": {
+"$t": "24"
+},
+"openSearch$startIndex": {
+"$t": "1"
+},
+"entry": [
+{
+"id": {
+"$t": "https://spreadsheets.google.com/feeds/list/1n7XUKOlf2n1Lw5r6A9YK8fJTIhiPHUwRM7oiZFagVds/od6/public/basic/cokwr"
+},
+"updated": {
+"$t": "2017-12-04T01:29:17.418Z"
+},
+"category": [
+{
+"scheme": "http://schemas.google.com/spreadsheets/2006",
+"term": "http://schemas.google.com/spreadsheets/2006#list"
+}
+],
+"title": {
+"type": "text",
+"$t": "total"
+},
+"content": {
+"type": "text",
+"$t": "miles: 27479"
+},
+"link": [
+{
+"rel": "self",
+"type": "application/atom+xml",
+"href": "https://spreadsheets.google.com/feeds/list/1n7XUKOlf2n1Lw5r6A9YK8fJTIhiPHUwRM7oiZFagVds/od6/public/basic/cokwr"
+}
+]
+},
+{
+"id": {
+"$t": "https://spreadsheets.google.com/feeds/list/1n7XUKOlf2n1Lw5r6A9YK8fJTIhiPHUwRM7oiZFagVds/od6/public/basic/cpzh4"
+},
+"updated": {
+"$t": "2017-12-04T01:29:17.418Z"
+},
+"category": [
+{
+"scheme": "http://schemas.google.com/spreadsheets/2006",
+"term": "http://schemas.google.com/spreadsheets/2006#list"
+}
+],
+"title": {
+"type": "text",
+"$t": "Walk"
+},
+"content": {
+"type": "text",
+"$t": "miles: 769"
+},
+"link": [
+{
+"rel": "self",
+"type": "application/atom+xml",
+"href": "https://spreadsheets.google.com/feeds/list/1n7XUKOlf2n1Lw5r6A9YK8fJTIhiPHUwRM7oiZFagVds/od6/public/basic/cpzh4"
+}
+]
+},
+{
+"id": {
+"$t": "https://spreadsheets.google.com/feeds/list/1n7XUKOlf2n1Lw5r6A9YK8fJTIhiPHUwRM7oiZFagVds/od6/public/basic/cre1l"
+},
+"updated": {
+"$t": "2017-12-04T01:29:17.418Z"
+},
+"category": [
+{
+"scheme": "http://schemas.google.com/spreadsheets/2006",
+"term": "http://schemas.google.com/spreadsheets/2006#list"
+}
+],
+"title": {
+"type": "text",
+"$t": "Airplane"
+},
+"content": {
+"type": "text",
+"$t": "miles: 19749"
+},
+"link": [
+{
+"rel": "self",
+"type": "application/atom+xml",
+"href": "https://spreadsheets.google.com/feeds/list/1n7XUKOlf2n1Lw5r6A9YK8fJTIhiPHUwRM7oiZFagVds/od6/public/basic/cre1l"
+}
+]
+},
+{
+"id": {
+"$t": "https://spreadsheets.google.com/feeds/list/1n7XUKOlf2n1Lw5r6A9YK8fJTIhiPHUwRM7oiZFagVds/od6/public/basic/chk2m"
+},
+"updated": {
+"$t": "2017-12-04T01:29:17.418Z"
+},
+"category": [
+{
+"scheme": "http://schemas.google.com/spreadsheets/2006",
+"term": "http://schemas.google.com/spreadsheets/2006#list"
+}
+],
+"title": {
+"type": "text",
+"$t": "Light Rail"
+},
+"content": {
+"type": "text",
+"$t": "miles: 168"
+},
+"link": [
+{
+"rel": "self",
+"type": "application/atom+xml",
+"href": "https://spreadsheets.google.com/feeds/list/1n7XUKOlf2n1Lw5r6A9YK8fJTIhiPHUwRM7oiZFagVds/od6/public/basic/chk2m"
+}
+]
+},
+{
+"id": {
+"$t": "https://spreadsheets.google.com/feeds/list/1n7XUKOlf2n1Lw5r6A9YK8fJTIhiPHUwRM7oiZFagVds/od6/public/basic/ciyn3"
+},
+"updated": {
+"$t": "2017-12-04T01:29:17.418Z"
+},
+"category": [
+{
+"scheme": "http://schemas.google.com/spreadsheets/2006",
+"term": "http://schemas.google.com/spreadsheets/2006#list"
+}
+],
+"title": {
+"type": "text",
+"$t": "Tuk Tuk"
+},
+"content": {
+"type": "text",
+"$t": "miles: 47"
+},
+"link": [
+{
+"rel": "self",
+"type": "application/atom+xml",
+"href": "https://spreadsheets.google.com/feeds/list/1n7XUKOlf2n1Lw5r6A9YK8fJTIhiPHUwRM7oiZFagVds/od6/public/basic/ciyn3"
+}
+]
+},
+{
+"id": {
+"$t": "https://spreadsheets.google.com/feeds/list/1n7XUKOlf2n1Lw5r6A9YK8fJTIhiPHUwRM7oiZFagVds/od6/public/basic/ckd7g"
+},
+"updated": {
+"$t": "2017-12-04T01:29:17.418Z"
+},
+"category": [
+{
+"scheme": "http://schemas.google.com/spreadsheets/2006",
+"term": "http://schemas.google.com/spreadsheets/2006#list"
+}
+],
+"title": {
+"type": "text",
+"$t": "Train"
+},
+"content": {
+"type": "text",
+"$t": "miles: 2406"
+},
+"link": [
+{
+"rel": "self",
+"type": "application/atom+xml",
+"href": "https://spreadsheets.google.com/feeds/list/1n7XUKOlf2n1Lw5r6A9YK8fJTIhiPHUwRM7oiZFagVds/od6/public/basic/ckd7g"
+}
+]
+},
+{
+"id": {
+"$t": "https://spreadsheets.google.com/feeds/list/1n7XUKOlf2n1Lw5r6A9YK8fJTIhiPHUwRM7oiZFagVds/od6/public/basic/clrrx"
+},
+"updated": {
+"$t": "2017-12-04T01:29:17.418Z"
+},
+"category": [
+{
+"scheme": "http://schemas.google.com/spreadsheets/2006",
+"term": "http://schemas.google.com/spreadsheets/2006#list"
+}
+],
+"title": {
+"type": "text",
+"$t": "Ferry"
+},
+"content": {
+"type": "text",
+"$t": "miles: 470"
+},
+"link": [
+{
+"rel": "self",
+"type": "application/atom+xml",
+"href": "https://spreadsheets.google.com/feeds/list/1n7XUKOlf2n1Lw5r6A9YK8fJTIhiPHUwRM7oiZFagVds/od6/public/basic/clrrx"
+}
+]
+},
+{
+"id": {
+"$t": "https://spreadsheets.google.com/feeds/list/1n7XUKOlf2n1Lw5r6A9YK8fJTIhiPHUwRM7oiZFagVds/od6/public/basic/cyevm"
+},
+"updated": {
+"$t": "2017-12-04T01:29:17.418Z"
+},
+"category": [
+{
+"scheme": "http://schemas.google.com/spreadsheets/2006",
+"term": "http://schemas.google.com/spreadsheets/2006#list"
+}
+],
+"title": {
+"type": "text",
+"$t": "Bus"
+},
+"content": {
+"type": "text",
+"$t": "miles: 2161"
+},
+"link": [
+{
+"rel": "self",
+"type": "application/atom+xml",
+"href": "https://spreadsheets.google.com/feeds/list/1n7XUKOlf2n1Lw5r6A9YK8fJTIhiPHUwRM7oiZFagVds/od6/public/basic/cyevm"
+}
+]
+},
+{
+"id": {
+"$t": "https://spreadsheets.google.com/feeds/list/1n7XUKOlf2n1Lw5r6A9YK8fJTIhiPHUwRM7oiZFagVds/od6/public/basic/cztg3"
+},
+"updated": {
+"$t": "2017-12-04T01:29:17.418Z"
+},
+"category": [
+{
+"scheme": "http://schemas.google.com/spreadsheets/2006",
+"term": "http://schemas.google.com/spreadsheets/2006#list"
+}
+],
+"title": {
+"type": "text",
+"$t": "Songthaew"
+},
+"content": {
+"type": "text",
+"$t": "miles: 201"
+},
+"link": [
+{
+"rel": "self",
+"type": "application/atom+xml",
+"href": "https://spreadsheets.google.com/feeds/list/1n7XUKOlf2n1Lw5r6A9YK8fJTIhiPHUwRM7oiZFagVds/od6/public/basic/cztg3"
+}
+]
+},
+{
+"id": {
+"$t": "https://spreadsheets.google.com/feeds/list/1n7XUKOlf2n1Lw5r6A9YK8fJTIhiPHUwRM7oiZFagVds/od6/public/basic/d180g"
+},
+"updated": {
+"$t": "2017-12-04T01:29:17.418Z"
+},
+"category": [
+{
+"scheme": "http://schemas.google.com/spreadsheets/2006",
+"term": "http://schemas.google.com/spreadsheets/2006#list"
+}
+],
+"title": {
+"type": "text",
+"$t": "Longtail Boat"
+},
+"content": {
+"type": "text",
+"$t": "miles: 16"
+},
+"link": [
+{
+"rel": "self",
+"type": "application/atom+xml",
+"href": "https://spreadsheets.google.com/feeds/list/1n7XUKOlf2n1Lw5r6A9YK8fJTIhiPHUwRM7oiZFagVds/od6/public/basic/d180g"
+}
+]
+},
+{
+"id": {
+"$t": "https://spreadsheets.google.com/feeds/list/1n7XUKOlf2n1Lw5r6A9YK8fJTIhiPHUwRM7oiZFagVds/od6/public/basic/d2mkx"
+},
+"updated": {
+"$t": "2017-12-04T01:29:17.418Z"
+},
+"category": [
+{
+"scheme": "http://schemas.google.com/spreadsheets/2006",
+"term": "http://schemas.google.com/spreadsheets/2006#list"
+}
+],
+"title": {
+"type": "text",
+"$t": "Scooter"
+},
+"content": {
+"type": "text",
+"$t": "miles: 333"
+},
+"link": [
+{
+"rel": "self",
+"type": "application/atom+xml",
+"href": "https://spreadsheets.google.com/feeds/list/1n7XUKOlf2n1Lw5r6A9YK8fJTIhiPHUwRM7oiZFagVds/od6/public/basic/d2mkx"
+}
+]
+},
+{
+"id": {
+"$t": "https://spreadsheets.google.com/feeds/list/1n7XUKOlf2n1Lw5r6A9YK8fJTIhiPHUwRM7oiZFagVds/od6/public/basic/cssly"
+},
+"updated": {
+"$t": "2017-12-04T01:29:17.418Z"
+},
+"category": [
+{
+"scheme": "http://schemas.google.com/spreadsheets/2006",
+"term": "http://schemas.google.com/spreadsheets/2006#list"
+}
+],
+"title": {
+"type": "text",
+"$t": "Minibus"
+},
+"content": {
+"type": "text",
+"$t": "miles: 820"
+},
+"link": [
+{
+"rel": "self",
+"type": "application/atom+xml",
+"href": "https://spreadsheets.google.com/feeds/list/1n7XUKOlf2n1Lw5r6A9YK8fJTIhiPHUwRM7oiZFagVds/od6/public/basic/cssly"
+}
+]
+},
+{
+"id": {
+"$t": "https://spreadsheets.google.com/feeds/list/1n7XUKOlf2n1Lw5r6A9YK8fJTIhiPHUwRM7oiZFagVds/od6/public/basic/cu76f"
+},
+"updated": {
+"$t": "2017-12-04T01:29:17.418Z"
+},
+"category": [
+{
+"scheme": "http://schemas.google.com/spreadsheets/2006",
+"term": "http://schemas.google.com/spreadsheets/2006#list"
+}
+],
+"title": {
+"type": "text",
+"$t": "Kayak"
+},
+"content": {
+"type": "text",
+"$t": "miles: 7"
+},
+"link": [
+{
+"rel": "self",
+"type": "application/atom+xml",
+"href": "https://spreadsheets.google.com/feeds/list/1n7XUKOlf2n1Lw5r6A9YK8fJTIhiPHUwRM7oiZFagVds/od6/public/basic/cu76f"
+}
+]
+},
+{
+"id": {
+"$t": "https://spreadsheets.google.com/feeds/list/1n7XUKOlf2n1Lw5r6A9YK8fJTIhiPHUwRM7oiZFagVds/od6/public/basic/cvlqs"
+},
+"updated": {
+"$t": "2017-12-04T01:29:17.418Z"
+},
+"category": [
+{
+"scheme": "http://schemas.google.com/spreadsheets/2006",
+"term": "http://schemas.google.com/spreadsheets/2006#list"
+}
+],
+"title": {
+"type": "text",
+"$t": "Scuba Gear"
+},
+"content": {
+"type": "text",
+"$t": "miles: 2"
+},
+"link": [
+{
+"rel": "self",
+"type": "application/atom+xml",
+"href": "https://spreadsheets.google.com/feeds/list/1n7XUKOlf2n1Lw5r6A9YK8fJTIhiPHUwRM7oiZFagVds/od6/public/basic/cvlqs"
+}
+]
+},
+{
+"id": {
+"$t": "https://spreadsheets.google.com/feeds/list/1n7XUKOlf2n1Lw5r6A9YK8fJTIhiPHUwRM7oiZFagVds/od6/public/basic/cx0b9"
+},
+"updated": {
+"$t": "2017-12-04T01:29:17.418Z"
+},
+"category": [
+{
+"scheme": "http://schemas.google.com/spreadsheets/2006",
+"term": "http://schemas.google.com/spreadsheets/2006#list"
+}
+],
+"title": {
+"type": "text",
+"$t": "Diving Boat"
+},
+"content": {
+"type": "text",
+"$t": "miles: 18"
+},
+"link": [
+{
+"rel": "self",
+"type": "application/atom+xml",
+"href": "https://spreadsheets.google.com/feeds/list/1n7XUKOlf2n1Lw5r6A9YK8fJTIhiPHUwRM7oiZFagVds/od6/public/basic/cx0b9"
+}
+]
+},
+{
+"id": {
+"$t": "https://spreadsheets.google.com/feeds/list/1n7XUKOlf2n1Lw5r6A9YK8fJTIhiPHUwRM7oiZFagVds/od6/public/basic/d9ney"
+},
+"updated": {
+"$t": "2017-12-04T01:29:17.418Z"
+},
+"category": [
+{
+"scheme": "http://schemas.google.com/spreadsheets/2006",
+"term": "http://schemas.google.com/spreadsheets/2006#list"
+}
+],
+"title": {
+"type": "text",
+"$t": "Bamboo Raft"
+},
+"content": {
+"type": "text",
+"$t": "miles: 2"
+},
+"link": [
+{
+"rel": "self",
+"type": "application/atom+xml",
+"href": "https://spreadsheets.google.com/feeds/list/1n7XUKOlf2n1Lw5r6A9YK8fJTIhiPHUwRM7oiZFagVds/od6/public/basic/d9ney"
+}
+]
+},
+{
+"id": {
+"$t": "https://spreadsheets.google.com/feeds/list/1n7XUKOlf2n1Lw5r6A9YK8fJTIhiPHUwRM7oiZFagVds/od6/public/basic/db1zf"
+},
+"updated": {
+"$t": "2017-12-04T01:29:17.418Z"
+},
+"category": [
+{
+"scheme": "http://schemas.google.com/spreadsheets/2006",
+"term": "http://schemas.google.com/spreadsheets/2006#list"
+}
+],
+"title": {
+"type": "text",
+"$t": "Car"
+},
+"content": {
+"type": "text",
+"$t": "miles: 13"
+},
+"link": [
+{
+"rel": "self",
+"type": "application/atom+xml",
+"href": "https://spreadsheets.google.com/feeds/list/1n7XUKOlf2n1Lw5r6A9YK8fJTIhiPHUwRM7oiZFagVds/od6/public/basic/db1zf"
+}
+]
+},
+{
+"id": {
+"$t": "https://spreadsheets.google.com/feeds/list/1n7XUKOlf2n1Lw5r6A9YK8fJTIhiPHUwRM7oiZFagVds/od6/public/basic/dcgjs"
+},
+"updated": {
+"$t": "2017-12-04T01:29:17.418Z"
+},
+"category": [
+{
+"scheme": "http://schemas.google.com/spreadsheets/2006",
+"term": "http://schemas.google.com/spreadsheets/2006#list"
+}
+],
+"title": {
+"type": "text",
+"$t": "Taxi"
+},
+"content": {
+"type": "text",
+"$t": "miles: 33"
+},
+"link": [
+{
+"rel": "self",
+"type": "application/atom+xml",
+"href": "https://spreadsheets.google.com/feeds/list/1n7XUKOlf2n1Lw5r6A9YK8fJTIhiPHUwRM7oiZFagVds/od6/public/basic/dcgjs"
+}
+]
+},
+{
+"id": {
+"$t": "https://spreadsheets.google.com/feeds/list/1n7XUKOlf2n1Lw5r6A9YK8fJTIhiPHUwRM7oiZFagVds/od6/public/basic/ddv49"
+},
+"updated": {
+"$t": "2017-12-04T01:29:17.418Z"
+},
+"category": [
+{
+"scheme": "http://schemas.google.com/spreadsheets/2006",
+"term": "http://schemas.google.com/spreadsheets/2006#list"
+}
+],
+"title": {
+"type": "text",
+"$t": "Bike"
+},
+"content": {
+"type": "text",
+"$t": "miles: 22"
+},
+"link": [
+{
+"rel": "self",
+"type": "application/atom+xml",
+"href": "https://spreadsheets.google.com/feeds/list/1n7XUKOlf2n1Lw5r6A9YK8fJTIhiPHUwRM7oiZFagVds/od6/public/basic/ddv49"
+}
+]
+},
+{
+"id": {
+"$t": "https://spreadsheets.google.com/feeds/list/1n7XUKOlf2n1Lw5r6A9YK8fJTIhiPHUwRM7oiZFagVds/od6/public/basic/d415a"
+},
+"updated": {
+"$t": "2017-12-04T01:29:17.418Z"
+},
+"category": [
+{
+"scheme": "http://schemas.google.com/spreadsheets/2006",
+"term": "http://schemas.google.com/spreadsheets/2006#list"
+}
+],
+"title": {
+"type": "text",
+"$t": "Inner tube"
+},
+"content": {
+"type": "text",
+"$t": "miles: 3"
+},
+"link": [
+{
+"rel": "self",
+"type": "application/atom+xml",
+"href": "https://spreadsheets.google.com/feeds/list/1n7XUKOlf2n1Lw5r6A9YK8fJTIhiPHUwRM7oiZFagVds/od6/public/basic/d415a"
+}
+]
+},
+{
+"id": {
+"$t": "https://spreadsheets.google.com/feeds/list/1n7XUKOlf2n1Lw5r6A9YK8fJTIhiPHUwRM7oiZFagVds/od6/public/basic/d5fpr"
+},
+"updated": {
+"$t": "2017-12-04T01:29:17.418Z"
+},
+"category": [
+{
+"scheme": "http://schemas.google.com/spreadsheets/2006",
+"term": "http://schemas.google.com/spreadsheets/2006#list"
+}
+],
+"title": {
+"type": "text",
+"$t": "Slow Boat"
+},
+"content": {
+"type": "text",
+"$t": "miles: 174"
+},
+"link": [
+{
+"rel": "self",
+"type": "application/atom+xml",
+"href": "https://spreadsheets.google.com/feeds/list/1n7XUKOlf2n1Lw5r6A9YK8fJTIhiPHUwRM7oiZFagVds/od6/public/basic/d5fpr"
+}
+]
+},
+{
+"id": {
+"$t": "https://spreadsheets.google.com/feeds/list/1n7XUKOlf2n1Lw5r6A9YK8fJTIhiPHUwRM7oiZFagVds/od6/public/basic/d6ua4"
+},
+"updated": {
+"$t": "2017-12-04T01:29:17.418Z"
+},
+"category": [
+{
+"scheme": "http://schemas.google.com/spreadsheets/2006",
+"term": "http://schemas.google.com/spreadsheets/2006#list"
+}
+],
+"title": {
+"type": "text",
+"$t": "Boat"
+},
+"content": {
+"type": "text",
+"$t": "miles: 40"
+},
+"link": [
+{
+"rel": "self",
+"type": "application/atom+xml",
+"href": "https://spreadsheets.google.com/feeds/list/1n7XUKOlf2n1Lw5r6A9YK8fJTIhiPHUwRM7oiZFagVds/od6/public/basic/d6ua4"
+}
+]
+},
+{
+"id": {
+"$t": "https://spreadsheets.google.com/feeds/list/1n7XUKOlf2n1Lw5r6A9YK8fJTIhiPHUwRM7oiZFagVds/od6/public/basic/d88ul"
+},
+"updated": {
+"$t": "2017-12-04T01:29:17.418Z"
+},
+"category": [
+{
+"scheme": "http://schemas.google.com/spreadsheets/2006",
+"term": "http://schemas.google.com/spreadsheets/2006#list"
+}
+],
+"title": {
+"type": "text",
+"$t": "Golf Cart"
+},
+"content": {
+"type": "text",
+"$t": "miles: 22"
+},
+"link": [
+{
+"rel": "self",
+"type": "application/atom+xml",
+"href": "https://spreadsheets.google.com/feeds/list/1n7XUKOlf2n1Lw5r6A9YK8fJTIhiPHUwRM7oiZFagVds/od6/public/basic/d88ul"
+}
+]
+},
+{
+"id": {
+"$t": "https://spreadsheets.google.com/feeds/list/1n7XUKOlf2n1Lw5r6A9YK8fJTIhiPHUwRM7oiZFagVds/od6/public/basic/dkvya"
+},
+"updated": {
+"$t": "2017-12-04T01:29:17.418Z"
+},
+"category": [
+{
+"scheme": "http://schemas.google.com/spreadsheets/2006",
+"term": "http://schemas.google.com/spreadsheets/2006#list"
+}
+],
+"title": {
+"type": "text",
+"$t": "Canoe"
+},
+"content": {
+"type": "text",
+"$t": "miles: 3"
+},
+"link": [
+{
+"rel": "self",
+"type": "application/atom+xml",
+"href": "https://spreadsheets.google.com/feeds/list/1n7XUKOlf2n1Lw5r6A9YK8fJTIhiPHUwRM7oiZFagVds/od6/public/basic/dkvya"
+}
+]
+}
+]
+}
+}
---
+++ positions.js
@@ -0,0 +1,36 @@
+positions_callback (
+{
+	"positions": [
+		{
+			"latitude":9.6350679 , 
+			"longitude":-85.1572434 , 
+			"timestamp": 1515101280540 ,
+		}, 
+		{
+			"latitude":9.6350677 , 
+			"longitude":-85.1572434 , 
+			"timestamp": 1515101290479 ,
+		}, 
+		{
+			"latitude":9.6350674 , 
+			"longitude":-85.1572436 , 
+			"timestamp": 1515101297538 ,
+		}, 
+		{
+			"latitude":9.6350673 , 
+			"longitude":-85.1572436 , 
+			"timestamp": 1515101302606 ,
+		}, 
+		{
+			"latitude":9.6350872 , 
+			"longitude":-85.1572352 , 
+			"timestamp": 1515101312448 ,
+		}, 
+	]
+})
+
+
+
+
+
+
